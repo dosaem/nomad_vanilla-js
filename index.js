ebd50639ecb3edcef728294aa95e9fd560fd2e72
dosaem90 <dosaem90@gmail.com>
2019-02-24 18:15:41 +0900

    2.4 event handlers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const title = document.querySelector("#title");
 
-title.innerHTML = "Hi! from JS";
-title.style.color = "red";
-document.title = "i am";
-console.dir(document);
+function handleClick(event) {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
