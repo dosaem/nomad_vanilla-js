1ce81b6ee7b8ebfc39d53468e99afdaf71c385ce
dosaem90 <dosaem90@gmail.com>
2019-02-25 02:31:08 +0900

    2.7 Funcion practice

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const title = document.querySelector("#title");
 
-function handleClick(event) {
-  title.style.color = "blue";
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+function init() {
+  title.addEventListener("click", handleClick);
 }
 
-title.addEventListener("click", handleClick);
+init();
